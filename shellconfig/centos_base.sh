d21a99c1b1946a914b52ebd39bc62d3baf4eba8a
Tigran Arabadjyan <arabadj@orgrimmar.local>
2020-04-13 12:27:29 +0400

    improve centos_base.sh.

--- shellconfig/centos_base.sh
+++ shellconfig/centos_base.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#wget https://github.com/arabadj/twink-lab-az/raw/master/shellconfig/centos_files/base.tar.gz
-
+# Disable Selinux
 setenforce 0
 
+# Stop and disable some services
 systemctl stop postfix
 systemctl disable postfix
 
@@ -16,12 +16,25 @@ systemctl disable rpcbind.socket
 systemctl stop NetworkManager
 systemctl disable NetworkManager
 
+# Clean yum database
 yum clean all
+
+# Update CentOS
 #yum update -y
+
+# Install some CentOS packages
 yum install mc -y
 
+# Base config CentOS
+cd /
+wget https://github.com/arabadj/twink-lab-az/raw/master/shellconfig/centos_files/base.tar.gz
+tar -zxvf base.tar.gz
+systemctl restart sshd
+
+# Install EPEL repository
 yum install epel-release -y
 
+# Install some packages
 yum install htop -y
 yum install iftop -y
 yum install nmon -y
