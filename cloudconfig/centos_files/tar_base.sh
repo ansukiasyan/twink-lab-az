a130a06f5d581de3cc9956d9f0c18a895505b639
Tigran Arabadjyan <arabadj@orgrimmar.local>
2020-04-14 08:33:35 +0400

    change centos clodconfig root settings.

--- cloudconfig/centos_files/tar_base.sh
+++ cloudconfig/centos_files/tar_base.sh
@@ -6,6 +6,7 @@ tar -czpf base.tar.gz \
 	/etc/selinux/config \
 	/etc/ssh/sshd_config \
 	/etc/sysconfig/network-scripts/ifcfg-eth0 \
+	/root/.ssh/authorized_keys \
 	/root/.config/mc/ini \
 	/root/.config/mc/panels.ini
 
